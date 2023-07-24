928f2b4b910daec39c8a4500330e49c9dd87fa87
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-24 20:09:52 +0530

    Added GET /launches

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors")
-
+const morgan = require("morgan");
 const planetsRouter = require("./routes/planets/planets.router")
+const launchesRouter = require("./routes/launches/launches.router")
 const app = express();
 app.use(cors({
   origin:"http://localhost:3000"
 }))
+app.use(morgan("short"))
 app.use(express.json());
 app.use(planetsRouter)
+app.use(launchesRouter)
 module.exports = app;
