45301ee5d204732cd43712e631fad2a7acc65207
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-24 18:31:06 +0530

    Planets Model and Loading Data

--- src/models/planets.model.js
+++ src/models/planets.model.js
@@ -1,9 +1,48 @@
+
+const { parse } = require("csv-parse");
+const path  = require("path")
+const fs = require("fs");
+const { resolve } = require("path");
+
+const habitablePlanets = [];
 const planets = [];
 
-function getAllPlanets() {
-  return planets
-}
 
+function isHabitable(planet) {
+  return planet["koi_disposition"] === "CONFIRMED" && planet["koi_insol"] > 0.36
+   && planet["koi_insol"]< 1.11  && planet["koi_prad"] < 1.6
+}
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+      .pipe(parse({
+        comment: "#",
+        columns: true
+      }))
+      .on("data", (data) => {
+        if (isHabitable(data))
+        {
+      
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+    
+        console.log(`Number of habitable planet is ${habitablePlanets.length}`)
+        resolve();
+      })
+  })
+  }
+ 
+
+ function getAllPlanets() {
+   return habitablePlanets;
+  }
 module.exports = {
-  getAllPlanets
+  getAllPlanets,
+  loadPlanetsData
 }
