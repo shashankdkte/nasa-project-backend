34ae02febbf950051abbab1930c0be388ac2a8ec
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-27 19:20:53 +0530

    Added mongodb connection

---
+++ src/services/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = 'mongodb+srv://shashankdkte:npJgoqJ6P6tR2fhE@nasacluster.pwrca91.mongodb.net/';
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+}
